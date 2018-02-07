eba0179a419519dabafe9378e06ca28468745a71
xuanvinh1997 <31702440+xuanvinh1997@users.noreply.github.com>
2018-02-07 18:06:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/xuanvinh/mychatbot/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.xuanvinh.mychatbot;
+
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.FuelError;
+import com.github.kittinunf.fuel.core.Handler;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.Intent;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+import com.ibm.watson.developer_cloud.http.ServiceCallback;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ConversationService myConversationService = null;
+    private TextView chatDisplayTV;
+    private EditText userStatementET;
+    private final String IBM_USERNAME = "e6d76627-c296-434c-a6b7-bc7160be7448";
+    private final String IBM_PASSWORD = "QRxcqAT6MQdG";
+    private final String IBM_WORKSPACE_ID = "883c59a5-af56-4654-8975-531a4e9da314";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chatDisplayTV = findViewById(R.id.conversation);
+        userStatementET = findViewById(R.id.user_input);
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, ifilter);
+        //get percent battery
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level / (float)scale;
+        //instantiating IBM Watson Conversation Service
+        myConversationService =
+                new ConversationService(
+                        "2017-12-06",
+                        IBM_USERNAME,
+                        IBM_PASSWORD
+                );
+        chatDisplayTV.setText("Hôm nay");
+        userStatementET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView tv, int action, KeyEvent keyEvent) {
+                if (action == EditorInfo.IME_ACTION_DONE) {
+                    //show the user statement
+                    final String userStatement = userStatementET.getText().toString();
+                    chatDisplayTV.append(
+                            Html.fromHtml("<p><b>YOU:</b> " + userStatement + "</p>")
+                    );
+                    userStatementET.setText("");
+
+                    MessageRequest request = new MessageRequest.Builder()
+                            .inputText(userStatement)
+                            .build();
+                    // initiate chat conversation
+                    myConversationService
+                            .message(IBM_WORKSPACE_ID, request)
+                            .enqueue(new ServiceCallback<MessageResponse>() {
+                                @Override
+                                public void onResponse(MessageResponse response) {
+                                    final String botStatement = response.getText().get(0);
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            chatDisplayTV.append(
+                                                    Html.fromHtml("<p><b>BOT:</b> " +
+                                                            botStatement + "</p>")
+                                            );
+                                        }
+                                    });
+
+                                }
+
+                                @Override
+                                public void onFailure(Exception e) {
+                                    Log.d(TAG, e.getMessage());
+                                }
+                            });
+                }
+                return false;
+            }
+        });
+    }
+}
